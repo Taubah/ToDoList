dd0f597b25d5182e5d66b7cbee758b18d60e2230
Taubah <Taubahpettus@gmailk.com>
2023-06-13 12:02:11 -0500

    n

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,16 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            HStack{
+                Text("To Do List")
+                    .font(.system(size : 40))
+                    .fontWeight(.black)
+               
+                Spacer()
+                
+                Button(action: {
+                                    
+                }) {
+                Text("+")
+                }
+                    .font(.system(size : 40))
+                    .fontWeight(.black)
+                    
+                
+                
+            }
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ ToDoList/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ToDoList
+//
+//  Created by Scholar on 6/13/23.
+//
+
+import Foundation
